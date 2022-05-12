a91c139b20bf350042cedc24d005726649d89b98
Jason Rowsell <76700693+jasonrowsell@users.noreply.github.com>
2022-05-12 19:57:01 +0100

    Sample data is randomized per call

--- sample/random/utils.go
+++ sample/random/utils.go
@@ -2,9 +2,14 @@ package random
 
 import (
 	"math/rand"
+	"time"
 
 	"github.com/google/uuid"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func RandomInt(min, max int) int {
 	return rand.Intn(max-min) + min
